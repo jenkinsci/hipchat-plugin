6052cbce78778912749a5afc75db4dc3d5ab943f
Peter Major <peter.major@forgerock.com>
2016-10-25 23:30:28 +0100

    JENKINS-27303 Add credential support to pipeline step impl

--- src/main/java/jenkins/plugins/hipchat/workflow/HipChatSendStep.java
+++ src/main/java/jenkins/plugins/hipchat/workflow/HipChatSendStep.java
@@ -3,12 +3,16 @@ package jenkins.plugins.hipchat.workflow;
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.Util;
+import hudson.model.Item;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+
+import hudson.util.ListBoxModel;
+import hudson.util.Secret;
 import jenkins.model.Jenkins;
 import jenkins.plugins.hipchat.HipChatNotifier;
 import jenkins.plugins.hipchat.HipChatService;
@@ -16,13 +20,17 @@ import jenkins.plugins.hipchat.Messages;
 import jenkins.plugins.hipchat.exceptions.NotificationException;
 import jenkins.plugins.hipchat.model.Color;
 import jenkins.plugins.hipchat.utils.BuildUtils;
+import jenkins.plugins.hipchat.utils.CredentialUtils;
 import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution;
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
 
 /**
  * Workflow step to send a HipChat room notification.
@@ -36,8 +44,15 @@ public class HipChatSendStep extends AbstractStepImpl {
     @DataBoundSetter
     public Color color;
 
+    /**
+     * @deprecated Use {@link #credentialId instead}.
+     */
+    @Deprecated
     @DataBoundSetter
     public String token;
+
+    @DataBoundSetter
+    public String credentialId;
 
     @DataBoundSetter
     public String room;
@@ -72,6 +87,11 @@ public class HipChatSendStep extends AbstractStepImpl {
             super(HipChatSendStepExecution.class);
         }
 
+        public ListBoxModel doFillCredentialIdItems(@AncestorInPath Item context, @QueryParameter String server) {
+            return Jenkins.getInstance().getDescriptorByType(HipChatNotifier.DescriptorImpl.class)
+                    .doFillCredentialIdItems(context, server);
+        }
+
         @Override
         public String getFunctionName() {
             return "hipchatSend";
@@ -90,6 +110,8 @@ public class HipChatSendStep extends AbstractStepImpl {
 
         @Inject
         private transient BuildUtils buildUtils;
+        @Inject
+        private transient CredentialUtils credentialUtils;
         @Inject
         private transient HipChatSendStep step;
         @StepContextParameter
@@ -113,10 +135,26 @@ public class HipChatSendStep extends AbstractStepImpl {
             //default to global config values if not set in step, but allow step to override all global settings
             HipChatNotifier.DescriptorImpl hipChatDesc =
                     Jenkins.getInstance().getDescriptorByType(HipChatNotifier.DescriptorImpl.class);
-            String token = step.token != null ? step.token : hipChatDesc.getToken();
+
             String room = step.room != null ? step.room : hipChatDesc.getRoom();
             String server = step.server != null ? step.server : hipChatDesc.getServer();
             String sendAs = step.sendAs != null ? step.sendAs : hipChatDesc.getSendAs();
+            String credentialId = step.credentialId;
+            String token = null;
+            if (StringUtils.isEmpty(credentialId)) {
+                if (StringUtils.isEmpty(step.token)) {
+                    credentialId = hipChatDesc.getCredentialId();
+                } else {
+                    token = step.token;
+                }
+            }
+
+            if (StringUtils.isNotEmpty(credentialId)) {
+                StringCredentials creds = credentialUtils.resolveCredential(run.getParent(), credentialId, server);
+                if (creds != null) {
+                    token = Secret.toString(creds.getSecret());
+                }
+            }
             //default to gray if not set in step
             Color color = step.color != null ? step.color : Color.GRAY;
             boolean v2enabled = step.v2enabled != null ? step.v2enabled : hipChatDesc.isV2Enabled();
